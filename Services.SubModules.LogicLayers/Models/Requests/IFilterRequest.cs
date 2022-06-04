2a11d2b56d4c3f85df12a982a8f8af8d8be80c2e
vtereshin <vtereshin@anresh.ru>
2022-06-04 10:03:16 +0300

    RTR-38 Рефакторинг сервисов

--- Services.SubModules.LogicLayers/Models/Requests/IFilterRequest.cs
+++ Services.SubModules.LogicLayers/Models/Requests/IFilterRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Services.SubModules.LogicLayers.Models.Requests
 {
-    public interface IFindRequest<T> where T : class
+    public interface IFilterRequest<T> where T : class
     {
         IQueryable<T> Find(IQueryable<T> result);
     }
