3ae762d9e9c68e15ad9826ea61f82c1cf12d864f
vtereshin <vtereshin@anresh.ru>
2022-06-04 10:00:47 +0300

    RTR-38 Рефакторинг сервисов

---
+++ Services.SubModules.LogicLayers/Models/Requests/Entities/FindRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.SubModules.LogicLayers.Models.Requests.Entities
+{
+    public abstract class FindRequest<T> : IFindRequest<T> where T : class
+    {
+        public abstract IQueryable<T> Find(IQueryable<T> result);
+    }
+}
---
+++ Services.SubModules.LogicLayers/Models/Requests/IFindRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.SubModules.LogicLayers.Models.Requests
+{
+    public interface IFindRequest<T> where T : class
+    {
+        IQueryable<T> Find(IQueryable<T> result);
+    }
+}
