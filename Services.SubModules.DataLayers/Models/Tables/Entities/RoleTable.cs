55b97c8bb96118bf9ebb8b2ab4a1755e2e2c7553
vtereshin <vtereshin@anresh.ru>
2022-06-04 16:10:16 +0300

    RTR-38 Рефакторинг сервисов

---
+++ Services.SubModules.DataLayers/Models/Tables/Entities/RoleTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+
+namespace Services.SubModules.DataLayers.Models.Tables.Entities
+{
+    public class RoleTable : IdentityRole<Guid>, ICreatedAtTable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonIgnore]
+        public override string NormalizedName 
+        {
+            get => base.NormalizedName;
+            set => base.NormalizedName = value;
+        }
+
+        public virtual DateTime? CreatedAt { get; set; }
+    }
+}
