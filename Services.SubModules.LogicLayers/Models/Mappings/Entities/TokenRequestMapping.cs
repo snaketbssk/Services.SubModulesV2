566bc5ba8fc024d2f73258573543f4939371b774
vtereshin <vtereshin@anresh.ru>
2022-06-06 15:33:49 +0300

    RTR-38 Рефакторинг сервисов

---
+++ Services.SubModules.LogicLayers/Models/Mappings/Entities/TokenRequestMapping.cs
@@ -0,0 +1,34 @@
+﻿using Services.SubModules.LogicLayers.Models.Requests;
+using Services.SubModules.LogicLayers.Models.Requests.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
+{
+    public class TokenRequestMapping : Mapping<ITokenRequest>
+    {
+        public string Token { get; set; }
+
+        public TokenRequestMapping(string token)
+        {
+            Token = token;
+        }
+
+        public override ITokenRequest Map()
+        {
+            var result = new TokenRequest
+            {
+                Token = Token
+            };
+            return result;
+        }
+
+        public override ITokenRequest Update(ITokenRequest result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
