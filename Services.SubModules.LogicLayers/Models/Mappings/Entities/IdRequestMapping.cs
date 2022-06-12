c1fbcc7537e16dc55b6200e2554c09a60d815f9e
vtereshin <vtereshin@anresh.ru>
2022-06-12 16:16:52 +0300

    RTR-38 Рефакторинг сервисов

--- Services.SubModules.LogicLayers/Models/Mappings/Entities/IdRequestMapping.cs
+++ Services.SubModules.LogicLayers/Models/Mappings/Entities/IdRequestMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Services.SubModules.LogicLayers.Models.Requests;
 using Services.SubModules.LogicLayers.Models.Requests.Entities;
+using Services.SubModules.Protos;
 
 namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
 {
@@ -10,6 +11,11 @@ namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
         public IdRequestMapping(Guid id)
         {
             Id = id;
+        }
+
+        public IdRequestMapping(UserIdentityGrpcRequest request)
+        {
+            Id = new Guid(request.Id.ToByteArray());
         }
 
         public override IIdRequest Map()
