686a9b0fbbd305ba7e20dd931481f8937c911de0
vtereshin <vtereshin@anresh.ru>
2022-07-07 00:33:20 +0300

    RTR-43 Добавить телефон, широту, долготу. Поправить поля, изменить их на nullable.

--- Services.SubModules.DataLayers/Constants/ContextConstant.cs
+++ Services.SubModules.DataLayers/Constants/ContextConstant.cs
@@ -3,6 +3,7 @@
     public static class ContextConstant
     {
         public const string DECIMAL = "decimal(18,8)";
+        public const string COORDINATES = "decimal(18,8)";
         public const string NEW_GUID = "NEWID()";
         public const string GET_UTC_DATE = "GETUTCDATE()";
     }
