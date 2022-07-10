69375816b60b1add7c70e999d3b4c54ce3190a34
vtereshin <vtereshin@anresh.ru>
2022-07-11 00:02:19 +0300

    RTR-49 Поправить реферальный токен

--- Services.SubModules.LogicLayers/Models/Requests/Entities/TokenRequest.cs
+++ Services.SubModules.LogicLayers/Models/Requests/Entities/TokenRequest.cs
@@ -4,7 +4,7 @@ namespace Services.SubModules.LogicLayers.Models.Requests.Entities
 {
     public class TokenRequest : ITokenRequest
     {
-        [StringLength(1024, MinimumLength = 1)]
+        [StringLength(1024)]
         public string? Token { get; set; }
 
         /// <summary>
@@ -17,7 +17,7 @@ namespace Services.SubModules.LogicLayers.Models.Requests.Entities
 
         public TokenRequest(string token)
         {
-            Token = token ?? throw new ArgumentNullException(nameof(token));
+            Token = token;
         }
     }
 }
