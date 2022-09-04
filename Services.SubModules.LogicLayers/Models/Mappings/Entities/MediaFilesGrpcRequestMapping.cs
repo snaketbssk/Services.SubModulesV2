4aa0c5e7693a6a089ecc106d64e7daaed4418af6
vtereshin <vtereshin@anresh.ru>
2022-09-05 00:25:03 +0300

    RTR-66 В сервисе Telegram добавить возможность добавление недвижимости

--- Services.SubModules.LogicLayers/Models/Mappings/Entities/MediaFilesGrpcRequestMapping.cs
+++ Services.SubModules.LogicLayers/Models/Mappings/Entities/MediaFilesGrpcRequestMapping.cs
@@ -1,8 +1,10 @@
-﻿using Services.SubModules.Protos;
+﻿using Google.Protobuf;
+using Services.SubModules.Protos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
@@ -10,19 +12,19 @@ namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
     public class MediaFilesGrpcRequestMapping : Mapping<MediaFilesGrpcRequest>
     {
         public string Type { get; set; }
-        public string Address { get; set; }
         public object Data { get; set; }
         public string Language { get; set; }
         public DateTime Date { get; set; }
+        public List<byte[]> Files { get; set; }
         public MediaFilesGrpcRequestMapping(
+            List<byte[]> files,
             string type,
-            string address,
             object data,
             string language,
             DateTime date)
-        { // 1
+        {
+            Files = files;
             Type = type;
-            Address = address;
             Data = data;
             Language = language;
             Date = date;
@@ -30,14 +32,21 @@ namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
         public override MediaFilesGrpcRequest Map()
         {
             var data = JsonSerializer.Serialize(Data);
-            var result = new MessageMailerGrpcRequest
+            var result = new MediaFilesGrpcRequest
             {
-                Type = Type ?? "",
-                Address = Address ?? "",
-                Data = data ?? "",
-                Date = Date.Ticks,
-                Language = Language ?? ""
+                Message = new MessageTelegramGrpcRequest
+                {
+                    Type = Type ?? "",
+                    Data = data ?? "",
+                    Date = Date.Ticks,
+                    Language = Language ?? ""
+                }
             };
+            result.Files.AddRange(Files.Select(x => new MessageFileGrpcRequest
+            {
+                Name = "1.webp",
+                Content = ByteString.CopyFrom(x)
+            }));
             return result;
         }
 
--- Services.SubModules.LogicLayers/Services/IStorageGrpcService.cs
+++ Services.SubModules.LogicLayers/Services/IStorageGrpcService.cs
@@ -7,5 +7,7 @@ namespace Services.SubModules.LogicLayers.Services
     {
         Task<StorageFileGrpcResponse> ExecuteAsync(IMapping<StorageFileGrpcRequest> mapping, CancellationToken cancellationToken = default);
         Task<StorageFilesGrpcResponse> ExecuteAsync(IMapping<StorageFilesGrpcRequest> mapping, CancellationToken cancellationToken = default);
+        Task<StorageReadFilesGrpcResponse> ExecuteAsync(IMapping<StorageReadFilesGrpcRequest> mapping, CancellationToken cancellationToken = default);
+        Task<StorageReadFileGrpcResponse> ExecuteAsync(IMapping<StorageReadFileGrpcRequest> mapping, CancellationToken cancellationToken = default);
     }
 }
