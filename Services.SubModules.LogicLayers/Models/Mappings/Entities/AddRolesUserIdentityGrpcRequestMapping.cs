98a0afa1306cf63ea30cb455686ec93f5a40f970
vtereshin <vtereshin@anresh.ru>
2022-09-27 23:35:32 +0400

    RTR-99 Рефакторинг Claims пользователя на беке

---
+++ Services.SubModules.LogicLayers/Models/Mappings/Entities/AddRolesUserIdentityGrpcRequestMapping.cs
@@ -0,0 +1,29 @@
+﻿using Google.Protobuf;
+using Services.SubModules.Protos;
+
+namespace Services.SubModules.LogicLayers.Models.Mappings.Entities
+{
+    public class AddRolesUserIdentityGrpcRequestMapping : Mapping<AddRolesUserIdentityGrpcRequest>
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public AddRolesUserIdentityGrpcRequestMapping(Guid id, IEnumerable<string> roles)
+        {
+            Id = id;
+            Roles = roles;
+        }
+        public override AddRolesUserIdentityGrpcRequest Map()
+        {
+            var result = new AddRolesUserIdentityGrpcRequest();
+            result.Id = ByteString.CopyFrom(Id.ToByteArray());
+            result.Roles.AddRange(Roles);
+
+            return result;
+        }
+
+        public override AddRolesUserIdentityGrpcRequest Update(AddRolesUserIdentityGrpcRequest result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
