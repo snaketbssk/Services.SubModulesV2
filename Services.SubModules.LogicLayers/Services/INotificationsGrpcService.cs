ffba1c665570438a8207957e9a8206071fec6167
vtereshin <vtereshin@anresh.ru>
2022-09-29 23:25:56 +0400

    RTR-102 BE: Создать новый сервис уведомлений

--- Services.SubModules.LogicLayers/Services/INotificationsGrpcService.cs
+++ Services.SubModules.LogicLayers/Services/INotificationsGrpcService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Services.SubModules.LogicLayers.Models.Mappings;
+using Services.SubModules.Protos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,8 @@ namespace Services.SubModules.LogicLayers.Services
 {
     public interface INotificationsGrpcService
     {
+        Task<EmptyNotificationsGrpcResponse> ExecuteAsync(IMapping<AddSuccessNotificationsGrpcRequest> mapping, CancellationToken cancellationToken = default);
+        Task<EmptyNotificationsGrpcResponse> ExecuteAsync(IMapping<AddWarningNotificationsGrpcRequest> mapping, CancellationToken cancellationToken = default);
+        Task<EmptyNotificationsGrpcResponse> ExecuteAsync(IMapping<AddErrorNotificationsGrpcRequest> mapping, CancellationToken cancellationToken = default);
     }
 }
