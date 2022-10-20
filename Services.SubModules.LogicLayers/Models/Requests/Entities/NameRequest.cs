1c951cfbb9598246fc9f07002517d3783be96c87
vtereshin <vtereshin@anresh.ru>
2022-10-20 08:36:45 +0400

    RTR-111 BE : Рефакторинг логгера.

--- Services.SubModules.LogicLayers/Models/Requests/Entities/NameRequest.cs
+++ Services.SubModules.LogicLayers/Models/Requests/Entities/NameRequest.cs
@@ -3,5 +3,14 @@
     public class NameRequest : INameRequest
     {
         public string Name { get; set; }
+
+        public NameRequest()
+        {
+        }
+
+        public NameRequest(string name)
+        {
+            Name = name;
+        }
     }
 }
