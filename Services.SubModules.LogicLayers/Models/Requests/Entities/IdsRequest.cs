25e8611870f39b43f4360fc5ceae2077e6882ee5
vtereshin <vtereshin@anresh.ru>
2022-11-23 14:49:31 +0400

    RTR-136 BE : Добавить Jenkins в сервисы

---
+++ Services.SubModules.LogicLayers/Models/Requests/Entities/IdsRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.SubModules.LogicLayers.Models.Requests.Entities
+{
+    public class IdsRequest : IIdsRequest
+    {
+        public List<IdRequest> Ids { get; set; }
+
+        public IdsRequest()
+        {
+            //
+        }
+
+        public IdsRequest(IdRequest id)
+        {
+            Ids = new List<IdRequest>() { id };
+        }
+
+        public IdsRequest(IEnumerable<IdRequest> ids)
+        {
+            Ids = new List<IdRequest>(ids);
+        }
+    }
+}
---
+++ Services.SubModules.LogicLayers/Models/Requests/IIdsRequest.cs
@@ -0,0 +1,14 @@
+﻿using Services.SubModules.LogicLayers.Models.Requests.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.SubModules.LogicLayers.Models.Requests
+{
+    public interface IIdsRequest
+    {
+        List<IdRequest> Ids { get; set; }
+    }
+}
