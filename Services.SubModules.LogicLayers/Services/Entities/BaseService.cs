d68a6a63a10bd24c5f0d54c1f67cf830a1b4c484
vtereshin <vtereshin@anresh.ru>
2022-12-01 16:01:08 +0400

    RTR-139 BE : Создать новый сервис Logger

---
+++ Services.SubModules.LogicLayers/Services/Entities/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Services.SubModules.LogicLayers.Services.Entities
+{
+    public abstract class BaseService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        protected BaseService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}
