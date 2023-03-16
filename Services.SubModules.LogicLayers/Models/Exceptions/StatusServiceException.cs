b46974aa48beefd4cbe25931c08463d13b943ae7
vtereshin <vtereshin@anresh.ru>
2023-03-16 10:26:54 +0400

    update

--- Services.SubModules.LogicLayers/Models/Exceptions/StatusServiceException.cs
+++ Services.SubModules.LogicLayers/Models/Exceptions/StatusServiceException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Services.SubModules.LogicLayers.Models.Exceptions
+﻿namespace Services.SubModules.LogicLayers.Models.Exceptions
 {
     public enum StatusServiceException
     {
